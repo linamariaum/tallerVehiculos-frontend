740517a08b51710bd95ad473580391d8d09de215
Lina Uribe <lina8452@gmail.com>
2020-10-18 17:51:16 -0500

    Install sweetAlert | Create color palette | Create toolbar in app.component with sticky | Update homePageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatGridListModule} from '@angular/material/grid-list';
 
 import { HomepageComponent } from './components/homepage/homepage.component';
 
@@ -21,7 +23,9 @@ import { HomepageComponent } from './components/homepage/homepage.component';
     BrowserAnimationsModule,
     MatSliderModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]

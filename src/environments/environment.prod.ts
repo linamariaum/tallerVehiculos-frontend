2f7d21a2287d00ff402261ed0d1f2f6d24e81826
Santiago Quintero <santiago.quinteroh@udea.edu.co>
2020-10-31 12:03:15 -0500

    serviceURL back

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serviceURL: 'http://ec2-34-207-62-212.compute-1.amazonaws.com:3000'
+  serviceURL: 'http://ec2-54-221-2-42.compute-1.amazonaws.com:3000',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  serviceURL: 'http://ec2-34-207-62-212.compute-1.amazonaws.com:3000'
+  serviceURL: 'http://ec2-54-221-2-42.compute-1.amazonaws.com:3000'
 };
 
 /*

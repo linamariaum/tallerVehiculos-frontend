5141e9d9622c28a06f66c44a1dd40898ab620615
Lina Uribe <lina8452@gmail.com>
2020-11-25 17:54:43 -0500

    Create employee-assistant component

---
+++ src/app/components/employee-assistant/employee-assistant.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-assistant',
+  templateUrl: './employee-assistant.component.html',
+  styleUrls: ['./employee-assistant.component.scss']
+})
+export class EmployeeAssistantComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

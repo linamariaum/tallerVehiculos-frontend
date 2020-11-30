0d0fcd0dbc251014eb39a10e5e42675d39ad6daa
Lina Uribe <lina8452@gmail.com>
2020-11-29 19:19:00 -0500

    Create Owner service

---
+++ src/app/services/owner.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { retry, catchError } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { throwError } from 'rxjs';
+import { Owner } from '../models/Owner';
+
+
+const urlMapping = '/owners';
+const urlBase = environment.serviceURL;
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OwnerService {
+  private urlApi = urlBase + urlMapping;
+
+  constructor(private http: HttpClient) { }
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+    }),
+    responseType: 'text' as 'json',
+  };
+
+  async getAllOwners(): Promise<Owner[]> {
+    return await this.http
+      .get<Owner[]>(this.urlApi)
+      .pipe(retry(1), catchError(this.handleError))
+      .toPromise();
+  }
+
+  async getOwner(id: string) {
+    return this.http
+      .get(this.urlApi + '/' + id);
+  }
+
+  async createOwner(employee: NewEmployeeRequests): Promise<any> {
+    return await this.http
+      .post<any>(this.urlApi, JSON.stringify(employee), this.httpOptions)
+      .pipe(retry(1), catchError(this.handleError))
+      .toPromise();
+  }
+
+  async removeOwner(id: string) {
+    return this.http.delete(this.urlApi + '/' + id);
+  }
+
+  async updateOwner(employee: UpdateEmployeeRequests): Promise<any> {
+    return await this.http
+      .put(this.urlApi + '/' + employee.id, employee)
+      .pipe(retry(1), catchError(this.handleError))
+      .toPromise();
+  }
+
+  // Error handling
+  handleError(error) {
+    let errorMessage;
+    if (error.error instanceof ErrorEvent) {
+      // Get client-side error
+      errorMessage = error.error.message;
+    } else {
+      // Get server-side error
+      // errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
+      errorMessage = error;
+    }
+    return throwError(errorMessage);
+  }
+}
